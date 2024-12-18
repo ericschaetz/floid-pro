4ec3353e18d4b872119918b6c54a2f2edbd8127c
ericschaetz <eric.schaetz@gmail.com>
2024-12-18 16:06:09 +0100

    Aktualisiere core.ts

--- core.ts
+++ core.ts
@@ -1,7 +1,7 @@
 /**
  * Custom blocks
  */
-//% weight=200 color=#0fbc11 icon="" block="FloidPro - Hauptsektion"
+//% weight=200 color=#0fbc11 icon="" block="FloidPro - Hauptsektion"
 //% groups="['Display', 'Bumper', 'Beleuchtung']"
 namespace Core {
 
