e191877ddca721d46f6589cccdbc39519419f28c
ericschaetz <eric.schaetz@gmail.com>
2024-12-18 16:12:22 +0100

    Aktualisiere motors.ts, front.ts, core.ts

--- front.ts
+++ front.ts
@@ -1,7 +1,7 @@
 /**
  * Custom blocks
  */
-//% weight=190 color=#0fbc11 icon="" block="FloidPro - Frontsektion"
+//% weight=190 color=#00688b icon="" block="FloidPro - Frontsektion"
 
 namespace Front {
     /**
--- motors.ts
+++ motors.ts
@@ -1,6 +1,6 @@
 /// <reference path="FloidPro.ts" />
 
-//% weight=180 color=#0fbc11 icon="" block="FloidPro - Antrieb"
+//% weight=180 color=#00688b icon="" block="FloidPro - Antrieb"
 namespace Motors{
     /**
      * Antrieb
