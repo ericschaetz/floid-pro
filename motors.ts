1bc39ce4563e81ee03b2353ad77e86a5b75c48dd
ericschaetz <eric.schaetz@gmail.com>
2024-12-18 16:00:56 +0100

    Aktualisiere motors.ts

--- motors.ts
+++ motors.ts
@@ -1,6 +1,6 @@
 /// <reference path="FloidPro.ts" />
 
-//% weight=180 color=#0fbc11 icon="🚗" block="FloidPro - Antrieb"
+//% weight=180 color=#0fbc11 icon="" block="FloidPro - Antrieb"
 namespace Motors{
     /**
      * Antrieb
