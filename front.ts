d1c93998bcf07fc1552082b0c50d1a0145196019
moritznachtigall <moritznachtigall@icloud.com>
2025-06-24 16:22:03 +0200

    Aktualisiere FloidPro.ts, front.ts

--- front.ts
+++ front.ts
@@ -61,13 +61,38 @@ namespace Front {
     export function sonar_switch(richtung:USSensor): void {
         if (advanced) errornode("Ultraschallweiche")
         let state = Core.i2cread(Controller_read.US)
-        Core.showNumber(state,3,1,1)
         let curr = state & 0b11111100
         let curr1 = curr | richtung 
         pins.i2cWriteNumber(62, curr1, NumberFormat.UInt8LE, false)
         readus = curr1
         basic.pause(5)
         
+    }
+
+    /**
+     * Stellt eine RGB-LED wie gewünscht ein
+     */
+    //% blockid="floidpro_rgb"
+    //% block="Stelle an RGB-LED &rgb den roten Anteil %r, den grünen Anteil %g und den blauen Anteil %b."
+    //% weight=84
+    //% group="Ultraschall und RGB"
+    export function rgb(rgb:RGB,r:OnOff,b:OnOff,g:OnOff){
+        if (advanced) errornode("RGB-LED")
+        let state = Core.i2cread(Controller_read.US)
+        let curr = 0
+        let richtung = 0
+        if (rgb==1){
+            let curr = state & 0b11111100
+            let richtung = (1 - r) * 4 + (1 - g) * 8 + (1 - b) * 16
+        }
+        else{
+            let curr = state & 0b11111100
+            let richtung = (1 - r) * 32 + (1 - g) * 64 + (1 - b) * 128
+        }
+        let curr1 = curr | richtung
+        pins.i2cWriteNumber(62, curr1, NumberFormat.UInt8LE, false)
+        readus = curr1
+        basic.pause(5)
     }
 
     /*Ende Ultraschall und RGB *******************************************************************************************************************************/
